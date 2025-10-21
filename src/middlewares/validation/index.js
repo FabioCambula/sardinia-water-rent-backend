69ecb88217e9ff84d9e5bd1f427bba2cfc9b5d80
FabioCambula <fabiocambulaa@yahoo.com>
2025-10-21 11:21:47 +0200

    fix middlewares

--- src/middlewares/validation/index.js
+++ src/middlewares/validation/index.js
@@ -1,7 +1,6 @@
 module.exports = {
   validateProduct: require("./validateProduct"),
   validateUser: require("./validateUser"),
-  validateOrder: require("./validateOrder"),
   validateCart: require("./validateCart"),
   validateBooking: require("./validateBooking"),
   isSelfOrAdmin: require("./isSelfOrAdmin"),
